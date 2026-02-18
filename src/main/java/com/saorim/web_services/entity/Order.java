6b6443166023df09c75a704d06cb63fad8749611
Saorim <meom@gft.com>
2026-02-18 15:14:41 -0300

    add OrderStatus

--- src/main/java/com/saorim/web_services/entity/Order.java
+++ src/main/java/com/saorim/web_services/entity/Order.java
@@ -4,6 +4,7 @@ import java.io.Serializable;
 import java.time.Instant;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.saorim.web_services.entity.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -12,18 +13,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Entity
 @Table(name = "tb_order")
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
 @EqualsAndHashCode(of = "id")
 public class Order implements Serializable {
 	private static final long serialVersionUID = -3403142678833549634L;
@@ -34,8 +27,57 @@ public class Order implements Serializable {
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant momento;
 	
-	//@JsonIgnore
+	private Integer status;
+	
 	@ManyToOne
 	@JoinColumn(name = "cliente_id")
 	private User cliente;
+	
+	public Order() {
+		// empty	
+	}
+	
+	public Order(Integer id, Instant momento, OrderStatus status, User cliente ) {
+		this.id = id;
+		this.momento = momento;
+		setStatus(status);
+		this.cliente = cliente;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public Instant getMomento() {
+		return momento;
+	}
+
+	public void setMomento(Instant momento) {
+		this.momento = momento;
+	}
+
+	public OrderStatus getStatus() {
+		return OrderStatus.valueOf(status);
+	}
+
+	public void setStatus(OrderStatus status) {
+		if(status != null) {
+			this.status = status.getCode();		
+		}
+	}
+
+	public User getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(User cliente) {
+		this.cliente = cliente;
+	}
+	
+	
+	
 }
