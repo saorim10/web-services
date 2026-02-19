59f81945aebc36568ceca0459af827be7ace735f
Saorim <meom@gft.com>
2026-02-19 15:41:22 -0300

    ManyToMany Product x Category

--- src/main/java/com/saorim/web_services/config/TestConfig.java
+++ src/main/java/com/saorim/web_services/config/TestConfig.java
@@ -54,6 +54,14 @@ public class TestConfig implements CommandLineRunner {
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 		
+		p1.getCategorias().add(cat2);
+		p2.getCategorias().add(cat1);
+		p2.getCategorias().add(cat3);
+		p3.getCategorias().add(cat3);
+		p4.getCategorias().add(cat3);
+		p5.getCategorias().add(cat2);
+		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 	}
 	
 
